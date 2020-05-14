fe8886f0b838b8502a8e6930bd9a231e2237679d
3gstudent <15814408+3gstudent@users.noreply.github.com>
2020-05-13 23:35:46 -0700

    Update Program.cs

--- SharpRDPCheck/SharpRDPCheck/Program.cs
+++ SharpRDPCheck/SharpRDPCheck/Program.cs
@@ -30,14 +30,14 @@ Note:
 The .NET Framework must support TLSv1.2.
 
 Usage:
-      SharpRDPBrute.exe <RDP ServerIP> <RDP ServerPort> <mode> <user> <password>
+      SharpRDPCheck.exe <RDP ServerIP> <RDP ServerPort> <mode> <user> <password>
       <mode>:
       - plaintext
       - ntlmhash
 
 Eg:
-      SharpRDPBrute.exe 192.168.1.1 3389 plaintext user1 password1
-      SharpRDPBrute.exe 192.168.1.1 3389 ntlmhash user1 c5a237b7e9d8e708d8436b6148a25fa1
+      SharpRDPCheck.exe 192.168.1.1 3389 plaintext user1 password1
+      SharpRDPCheck.exe 192.168.1.1 3389 ntlmhash user1 c5a237b7e9d8e708d8436b6148a25fa1
 
 ";
             Console.WriteLine(Usage);
